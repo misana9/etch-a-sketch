8003ae945a74fe6f357fb0ff1a97bada4ba1dce9
JD <once.jd2@gmail.com>
2024-08-02 15:14:34 +0800

    changing the color on mouse hover

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 const gridBox = document.getElementById("grid-container");
-let input = 16;
+let input = 64;
 let gridBoxSize=input * input;
 let boxSize = (600 / input)-2;
 
 function createGrids(gridBoxSize){
     for(let i=0;i <gridBoxSize;i++){
         const grid = document.createElement("div");
+        grid.setAttribute("id", "uncoloredBox")
         grid.style.height = `${boxSize}px`;
         grid.style.width = `${boxSize}px`;
-        grid.textContent = i;
         grid.style.flex = "0 0 auto"
         grid.style.border = "solid";
         grid.style.borderWidth = "1px"
         gridBox.appendChild(grid);
     }
 }
-
 createGrids(gridBoxSize);
+
+const coloring = document.querySelectorAll("#uncoloredBox")
+coloring.forEach(function(grid){
+    grid.addEventListener("mouseover", (e) =>{
+        e.currentTarget.style.backgroundColor = "white";
+    })
+})
