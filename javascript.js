6e4ec1b62c6696c89902602ca0dba871a493e78f
JD <once.jd2@gmail.com>
2024-08-05 15:31:28 +0800

    added buttons

--- javascript.js
+++ javascript.js
@@ -1,25 +1,61 @@
 const gridBox = document.getElementById("grid-container");
-let input = 64;
-let gridBoxSize=input * input;
+const userInput = document.getElementById("item");
+const button = document.getElementById("createGrid");
+const resetDiv = document.getElementById("remake");
+let input;
+
+button.addEventListener("click",()=> {
+    input = userInput.value;
+    createGrids(input);
+    userInput.value = "";
+    button.style.visibility = "collapse";
+
+    const reset = document.getElementById("reset");
+    reset.style.visibility = "visible";
+    reset.addEventListener("click",() =>{
+        const gridReset = document.querySelectorAll(".uncoloredBox");
+        gridReset.forEach((e) =>{
+            e.style.backgroundColor = "grey";
+        })
+    })
+    userInput.addEventListener("input",() =>{
+        reset.style.visibility = "hidden";
+        button.style.visibility = "visible";
+    })
+})
+
+
+
+function createGrids(input){
+let gridBoxSize = input * input;
 let boxSize = (600 / input)-2;
 
-function createGrids(gridBoxSize){
     for(let i=0;i <gridBoxSize;i++){
         const grid = document.createElement("div");
-        grid.setAttribute("id", "uncoloredBox")
+        grid.setAttribute("class", "uncoloredBox")
         grid.style.height = `${boxSize}px`;
         grid.style.width = `${boxSize}px`;
         grid.style.flex = "0 0 auto"
         grid.style.border = "solid";
         grid.style.borderWidth = "1px"
         gridBox.appendChild(grid);
+
+        const coloring = document.querySelectorAll(".uncoloredBox");
+        coloring.forEach(function(grid){
+            grid.addEventListener("mouseover",() =>{
+                grid.style.backgroundColor = "white";
+                })
+        })
+
+        resetDiv.addEventListener("click",() => {
+            gridBox.removeChild(grid);
+            button.style.visibility = "visible";
+            reset.style.visibility = "hidden";
+        })
     }
 }
-createGrids(gridBoxSize);
 
-const coloring = document.querySelectorAll("#uncoloredBox")
-coloring.forEach(function(grid){
-    grid.addEventListener("mouseover", (e) =>{
-        e.currentTarget.style.backgroundColor = "white";
-    })
-})
+
+
+
+
